723b0ff5def4f650678276ed3c6decdf21614def
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-10-11 11:12:04 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRouter);
+app.use('/shop',shopRouter);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found </h1>')
+})
+
+app.listen(4000);
